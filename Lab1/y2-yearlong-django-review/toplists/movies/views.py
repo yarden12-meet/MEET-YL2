5543ed5cd39e579be59e40ce2b3d2328fddf4c64
Yarden Ben Ami <yarden12@meet.mit.edu>
2013-10-27 19:34:28 +0200

    Corrected a bug.

--- Lab1/y2-yearlong-django-review/toplists/movies/views.py
+++ Lab1/y2-yearlong-django-review/toplists/movies/views.py
@@ -21,7 +21,7 @@ def add_movie(request):
 
 def movie(request, movie_id):
     current_movie = Movie.objects.filter(pk = movie_id)[0]
-    commentslist=Comment.objects.all()
+    commentslist=Comment.objects.filter(movie=current_movie)
     context = {'movie':current_movie, 'comments':commentslist}
     return render(request, 'movies/movie.html', context)
 
