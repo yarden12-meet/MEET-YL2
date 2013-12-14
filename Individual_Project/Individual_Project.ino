657d72adec6a57bccf3f07dfb3c7b0d6912a2f3e
yarden12-meet <yardenba030@gmail.com>
2013-12-14 23:34:37 +0200

    basic project

--- Individual_Project/Individual_Project.ino
+++ Individual_Project/Individual_Project.ino
@@ -3,35 +3,32 @@ void setup() {
   pinMode(2, INPUT);
   int buttonPin = 2;
   
-  int binNum [4]; // this array will store the binary number
+  int binNum [4]; // this array will store the digits of the binary number
   int buttonInput = 0; // will store the button state
-  
-  
-  
-Serial.println("Press the button when you are ready!");
-while (buttonInput!=1) // this loop wil run until the user will push the button
-{
-  buttonInput = digitalRead(buttonPin);
-}
 
+
+  Serial.println("Press the button when you are ready!");
+  while (buttonInput!=1) // this loop wil run until the user will push the button
+  {
+    buttonInput = digitalRead(buttonPin);
+  }
   Serial.println("started");
-  // Serial.println(pow(2,3));
+  
+  
+  //
+  // Now the program starts
+  //
+  
   
   for(int i = 0; i<4; i++) // the proccess of recieving input will repeat 4 times
   {
-    delay(2000);
-    
-   
-    
-    
-    
-    
+    delay(2000); // waits 2 seconds before each time the user is asked for input
     Serial.print("Your input --->");
     
     for(long j = 0; j<200000; j++) // this loop is intended to give the user enough time to push the button
     {
         buttonInput = digitalRead(buttonPin); // reads the state of the button
-    
+        
         if (buttonInput == 1) // if the button is pushed the loop breaks
         {
           break;
@@ -40,35 +37,32 @@ while (buttonInput!=1) // this loop wil run until the user will push the button
     
     // now buttonInput is 1 if the button was pushed, and 0 otherwise
     
-    if (buttonInput==1)
+    if (buttonInput==1) // if the button was pushed
     {
      Serial.println("pushed");
      binNum[i] = 1;
     }
-    else
+    else // if the button was not pushed
     {
       Serial.println("not pushed");
       binNum[i] = 0;
-    }
-    
-    
-    
+    }  
   }
   
+  
   Serial.print("Your input number is: ");
-  for(int i = 0; i<4; i++)
+  for(int i = 0; i<4; i++) // this loop prints the binary number
   { 
     Serial.print(binNum[i]);
   }
   Serial.println();
   
-//  int dec = 0; // the decimal number
-//  for(int i = 0; i<4; i++)
-//  { 
-//    dec+=binNum[i]*pow(2,(3-i));
-//  }
-//  
-//  Serial.print(dec);
+  double dec = 0; // the decimal number
+  for(int i = 0; i<4; i++) // this loop converts the binary number into a decimal number
+  {
+    dec+=binNum[i]*pow(2,(3-i));
+  }
+  Serial.println(dec);
 
 }
 
